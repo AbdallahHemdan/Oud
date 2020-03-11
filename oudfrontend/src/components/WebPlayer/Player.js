0a83c6edf50383ddabb8450f1f6b7de3b8728665
aashrafh <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-03-11 22:57:18 +0200

    initial web player

---
+++ oudfrontend/src/components/WebPlayer/Player.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Player.css";
+class WebPlayer extends Component {
+  state = {};
+  render() {
+    return <div className="now-playing-bar-container"></div>;
+  }
+}
+
+export default WebPlayer;
