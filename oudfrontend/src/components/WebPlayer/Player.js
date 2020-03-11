ed8c987f6660a9e8c2ad01455a64385fea6fee7d
aashrafh <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-03-11 23:51:46 +0200

    Division of the player

--- oudfrontend/src/components/WebPlayer/Player.js
+++ oudfrontend/src/components/WebPlayer/Player.js
@@ -3,7 +3,15 @@ import "./Player.css";
 class WebPlayer extends Component {
   state = {};
   render() {
-    return <div className="now-playing-bar-container"></div>;
+    return (
+      <div className="now-playing-bar-container">
+        <div className="now-playing-bar">
+          <div className="now-playing-bar-left"></div>
+          <div className="now-playing-bar-center"></div>
+          <div className="now-playing-bar-right"></div>
+        </div>
+      </div>
+    );
   }
 }
 
