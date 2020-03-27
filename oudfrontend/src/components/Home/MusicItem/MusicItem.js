99f4dd9eb761f2e54678fba595976fe24661cc24
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-03-27 02:45:27 +0200

    💡 Add Functional documentation of the Music Item

--- oudfrontend/src/components/Home/MusicItem/MusicItem.js
+++ oudfrontend/src/components/Home/MusicItem/MusicItem.js
@@ -3,8 +3,24 @@ import "./MusicItem.css";
 import MusicCard from "../MusicCard/MusicCard"
 import { Link } from "react-router-dom";
 
-
+/**
+ * component to render all the Category stuff which is a list of playlist associated with this category
+ * 
+ * @author Abdallah Hemdan
+ * 
+ * @component
+ * 
+ */
 class MusicItem extends Component {
+  /**
+   * @constructor
+   * 
+   * @param {object} props - get props from higher components (Home)
+   * @param {object} item - get item from higher component (Home)
+   * @param {object} id - get id of an item from higher component (Home)
+   * @param {object} name - get name of an item from higher component (Home)
+   * @param {object} icon - get icon of an item from higher component (Home)
+   */
   constructor(props) {
     super(props)
 
@@ -12,16 +28,42 @@ class MusicItem extends Component {
     const { id, name, icon } = this.props.item;
 
     this.state = {
-      id: id, // id of the category 
-      name: name, // name of the category 
-      icon: icon // icon of the category 
+      /**
+       * id of the category
+       * 
+       * @type {string} 
+       */
+
+      id: id,
+
+      /**
+       * name of the category
+       *
+       * @type {string}
+       */
+      name: name,
+
+      /**
+       * icon of the category
+       *
+       * @type {string}
+       */
+      icon: icon
     }
   }
 
+  // Fetching data immediately after the component has been mount to the DOM tree
   componentDidMount() {
 
   }
 
+  /**
+     * @function
+     * @name render
+     * @description Render all the playlists of a specific category which it's defined by its id 
+     * 
+     * @returns {JSX} Component for Home
+     */
   render() {
     return (
       <div className="module">
