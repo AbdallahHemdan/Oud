371f615d4f13bbd080cfd84ff3d96e68d342dc92
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-03-27 02:44:57 +0200

    💡 Add Functional Documentation of the music card

--- oudfrontend/src/components/Home/MusicCard/MusicCard.js
+++ oudfrontend/src/components/Home/MusicCard/MusicCard.js
@@ -1,14 +1,67 @@
 import React, { Component } from "react";
 import "./MusicCard.css";
 import { Link, withRouter } from "react-router-dom"
+
+
+/**
+ * Music card component which render and display the playlist card of a specific category 
+ * 
+ * @author Abdallah Hemdan
+ * 
+ * @component
+ * 
+ */
+
 class MusicCard extends Component {
-  handlePlaylistClick = (e) => {
+  /**
+   * @constructor
+   * 
+   * @param {object} props - get musicItem (playlist date and props from the MusicItems component)
+   */
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  /**
+   * Function to handle navigation to the playlist page
+   * on clicking on the music card
+   * 
+   * @function
+   * 
+   * @return {void}
+   * 
+   */
+  handlePlaylistClick = () => {
     this.props.history.push('playlist');
   }
+
+  /**
+   * Function to handle playing music on clicking on
+   * play icon the music card
+   * 
+   * @function
+   * 
+   * @param {object} event - an event to use it in
+   * disabling the default of the propagation 
+   */
   handlePlayClick = (e) => {
     e.stopPropagation();
     console.log("🎵 music is playing now");
   }
+
+  /**
+   * @function
+   * 
+   * @name render
+   * 
+   * @description Render Music card components..
+   * 
+   * @returns {JSX} Component for App
+   */
   render() {
     return (
       <div className="card-container">
