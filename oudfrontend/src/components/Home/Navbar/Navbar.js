40ee2b796b8e8178db271fcda7644c62e039af14
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-03-27 02:45:54 +0200

    💡 Add Functional documentation of the Navbar

--- oudfrontend/src/components/Home/Navbar/Navbar.js
+++ oudfrontend/src/components/Home/Navbar/Navbar.js
@@ -3,27 +3,79 @@ import "./Navbar.css";
 import { withRouter } from "react-router-dom";
 import { BeforeLogin, AfterLogin } from "./BeforeAfterLogin"
 
+/**
+ * Component to render all the stuff in Home page
+ *
+ *
+ * @author Abdallah Hemdan
+ *
+ * @component
+ *
+ */
 
 class Navbar extends Component {
+  /**
+   * @constructor
+   *
+   * @param {object} props - get props from higher components
+   * @param {boolean} isLoggedIn - bool to indicate wether a use is logged in or not
+   */
+
   constructor(props) {
     super(props)
     this.state = {
+      /**
+       * A variable to check if the user is logged or not to use it in enabling or disabling some data and navbar view
+       * 
+       * @property {boolean} 
+       * 
+       */
       isLoggedIn: true
     }
   }
 
-  handleClickOnSearch = () => {
-    this.props.history.replace('search');
+  /**
+   * A function to handle switch current route to search route on clicking in search bar
+   *
+   * @function
+   *
+   * @param {string} newRoute - new route to change to it
+   * 
+   * @returns {void} returns nothing, it just handle changing the routes
+   */
+  handleClickOnSearch = (newRoute) => {
+    this.props.history.replace({ newRoute });
   }
 
+  /**
+   * A function to handle going back to last route we were used
+   * 
+   * @function
+   * 
+   * @returns {void} returns nothing, it just handle changing the routes to last one back
+   * 
+   */
   handleGoBack = () => {
     this.props.history.goBack();
   }
 
+  /**
+   * A function to handle going forward to the last route we were used
+   * 
+   * @function
+   * 
+   * @returns {void } returns nothing, it just handle changing the routes to last one forward
+   */
   handleGoForward = () => {
     this.props.history.goForward();
   }
 
+  /**
+   * @function
+   * @name render
+   * @description Render all Navbar components
+   * @returns {JSX} Component for Home
+   */
   render() {
     return (
       <div>
@@ -43,7 +95,7 @@ class Navbar extends Component {
               id="iconified"
               placeholder="&#xF002; Search for Artists, Songs"
               aria-label="Search"
-              onClick={this.handleClickOnSearch}
+              onClick={() => this.handleClickOnSearch('search')}
             />
           </form>
           <button
