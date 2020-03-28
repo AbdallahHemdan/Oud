b53b2909c3be835080f48e0cd40d306131da88af
aashrafh <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-03-28 02:05:49 +0200

    Track Component

--- oudfrontend/src/components/WebPlayer/Queue/SortableTrackContainer.jsx
+++ oudfrontend/src/components/WebPlayer/Queue/SortableTrackContainer.jsx
@@ -1,47 +1,8 @@
 import React from "react";
-import { SortableElement, sortableHandle } from "react-sortable-hoc";
-import art from "../../../assets/images/icons/album.jpg";
-import ellipsis from "../../../assets/images/icons/ellipsis.png";
-import handler from "../../../assets/images/icons/handler.png";
-const DragHandle = sortableHandle(() => (
-  <span className="handler">
-    <img src={handler} alt="Handler" />
-  </span>
-));
+import { SortableElement } from "react-sortable-hoc";
+import Track from "./Track";
 const SortableTrackContainer = SortableElement(() => {
-  return (
-    <div className="track">
-      <DragHandle />
-      <div className="content">
-        <div
-          className="track-art-work"
-          style={{ backgroundImage: `url(${art})` }}
-        ></div>
-
-        <div className="track-name">
-          <text title="Somthing Just Like This">
-            <a href="https://www.facebook.com/">Somthing Just Like This</a>
-          </text>
-        </div>
-
-        <div className="artist-name">
-          <text title="The Chainsmokers & Coldplay">
-            <a href="https://www.facebook.com/">The Chainsmokers & Coldplay</a>
-          </text>
-        </div>
-
-        <div className="duration">
-          <text>3:21</text>
-        </div>
-
-        <div className="ellipsis-container">
-          <button className="ellipsis-icon">
-            <img src={ellipsis} alt="Show More" />
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  return <Track />;
 });
 
 export default SortableTrackContainer;
--- oudfrontend/src/components/WebPlayer/Queue/Track.jsx
+++ oudfrontend/src/components/WebPlayer/Queue/Track.jsx
@@ -1,7 +1,47 @@
 import React from "react";
-
+import { sortableHandle } from "react-sortable-hoc";
+import art from "../../../assets/images/icons/album.jpg";
+import ellipsis from "../../../assets/images/icons/ellipsis.png";
+import handler from "../../../assets/images/icons/handler.png";
+const DragHandle = sortableHandle(() => (
+  <span className="handler">
+    <img src={handler} alt="Handler" />
+  </span>
+));
 const Track = props => {
-  return <div className="track"></div>;
+  return (
+    <div className="track">
+      <DragHandle />
+      <div className="content">
+        <div
+          className="track-art-work"
+          style={{ backgroundImage: `url(${art})` }}
+        ></div>
+
+        <div className="track-name">
+          <text title="Somthing Just Like This">
+            <a href="https://www.facebook.com/">Somthing Just Like This</a>
+          </text>
+        </div>
+
+        <div className="artist-name">
+          <text title="The Chainsmokers & Coldplay">
+            <a href="https://www.facebook.com/">The Chainsmokers & Coldplay</a>
+          </text>
+        </div>
+
+        <div className="duration">
+          <text>3:21</text>
+        </div>
+
+        <div className="ellipsis-container">
+          <button className="ellipsis-icon">
+            <img src={ellipsis} alt="Show More" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Track;
