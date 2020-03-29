4df540e412eda471ca091475b9151d6d245fec4f
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-03-29 04:26:02 +0200

    🔧 Add testing configurations

--- oudfrontend/src/App.js
+++ oudfrontend/src/App.js
@@ -3,23 +3,20 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import Search from "./pages/Search/Search"
 import {
-  BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/search">
-          <Search />
-        </Route>
-      </Switch>
-    </Router >
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/search">
+        <Search />
+      </Route>
+    </Switch>
   );
 }
 
--- oudfrontend/src/index.js
+++ oudfrontend/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from "react-router-dom"
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root'));
 serviceWorker.unregister();
