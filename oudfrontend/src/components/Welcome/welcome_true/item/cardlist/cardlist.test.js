b81210785b3309cc279c3ca53b319723e6e39bc7
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2020-04-16 19:58:29 +0200

    Add the body and the cardlist

---
+++ oudfrontend/src/components/Welcome/welcome_true/item/cardlist/cardlist.test.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import {render} from '@testing-library/react';
+// import * as renderer from 'react-test-renderer';
+// import {shallow} from 'enzyme';
+// import '../../../../testSetup';
+// import '@testing-library/jest-dom/extend-expect';
+// import CardList from './cardlist.js';
+
+// const setUP = (props = {}) => {
+//   const component = shallow(<CardList />);
+//   return component;
+// };
+
+// describe('Cardlist page testing ', () => {
+//   let component;
+//   beforeEach(() => {
+//     component = setUP();
+//   });
+//   it('render a background ', () => {
+//     const component = shallow(<CardList />);
+//     const logo = component.find(`[data-testid="CardImage"]`);
+//     expect(logo.length).toBe(6);
+//   });
+// });
