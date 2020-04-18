d05402873bdaf1a16394e5f05b2aeef2834def44
Ahmed Mahboub <43186742+Mahboub99@users.noreply.github.com>
2020-04-18 16:53:24 +0200

    Profile (#24)
    * 1)restructure the code ,put account with commponents
    2)Create Profile folder in Components
    
    * Add all routes
    
    1) overview
    2)followers
    3)following
    4)public playlists
    
    * Profile Completed without Unit testing and functions dos
    
    1)user Followers
    2)user Following
    3)Profile over view
    4)Get public playlists
    5)Navigate to another user account
    
    * Profile Without functional docs
    
    1)add  change user image
    2)unit testing
    
    * edit confirm password in Account
    
    Make it uncontrolled componenet
    
    * review process
    
    1)add see all link
    2)handle change image error in cancle
    
    * Integration Prep
    
    1)delete all not needed files
    2)Make all Moking in on db.json file
    3)put all routes in one routes.json file
    4)write script to run the mock on 2022
      port
    =========================
    how to run mock
    ---------------------
    1)cd src/api
    2)yarn run json:server
    
    * Edit Profile and Account structur
    
    *put Account and profile components in
     pages folder
    
    * Update db.json
    
    * Update db.json
    
    * Update routes.json
    
    * fix bugs
    
    change webplayer colors
    edit country value to be 2 char code
    -----------------------------------------
    
    * fix bug
    
    fix change password in account wen copying into it
    
    * add functions docs to profile

--- oudfrontend/src/components/Profile/Commponents/FollowCard/FollowCard.jsx
+++ oudfrontend/src/components/Profile/Commponents/FollowCard/FollowCard.jsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 import "./FollowCard.css";
-
+/**
+ * @type {Class}
+ *
+ * @returns {JSX} the card that renders the person who is in followers list or the following list
+ */
 class FollowCard extends Component {
   constructor(props) {
     super(props);
@@ -59,7 +63,7 @@ class FollowCard extends Component {
   handleClick(event) {
     /*
       1) make put request if it was false 
-      2) make delet request if true
+      2) make delete request if true
       3) then change the state
     */
     let ids = this.props.id;
@@ -107,7 +111,7 @@ class FollowCard extends Component {
         <div className="followCard-content">
           <Link
             id={"user" + this.props.id}
-            to={`/profile/${this.props.id}`}
+            to={`/profile/${this.props.id}/overview`}
             className="userName-followCard"
             data-test="followCardName"
           >
--- oudfrontend/src/components/likedSongs/components/headerBody.jsx
+++ oudfrontend/src/components/likedSongs/components/headerBody.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom'
-import PropTypes from 'prop-types';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import PropTypes from "prop-types";
 /**
  * this is a component that renders the bottom of the body of playlists, albums, likedSongs
  * @author Ahmed Walid <ahmedwa1999@gmail.com>
@@ -16,29 +16,42 @@ import PropTypes from 'prop-types';
  */
 
 function HeaderBody(props) {
-    const { length, playClicked, playing } = props;
-    let history = useHistory()
-    return (
-        <div data-testid="HeaderBody">
-            <h2 data-testid="title" className='gray-text likedSongsTitle'>Liked Songs</h2>
-            <button data-testid="owner" className='playlistAnchor songButton block' onClick={() => { history.push('/user/1') }}>Ahmed{/*userName*/}</button>
-            <button onClick={playClicked} data-testid="playButton" className="playButton" variant="outline-success">
-                {playing ? 'PAUSE' : 'PLAY'}
-            </button>
-            <p className='likedSongsTitle gray-text' > 
-                <span data-testid="songsNumber">{length} </span>
-                <span data-testid="songsLiteral">{length > 1 ? 'songs' : 'song'}</span>
-            </p>
-
-        </div>
-    );
+  const { length, playClicked, playing } = props;
+  let history = useHistory();
+  return (
+    <div data-testid="HeaderBody">
+      <h2 data-testid="title" className="gray-text likedSongsTitle">
+        Liked Songs
+      </h2>
+      <button
+        data-testid="owner"
+        className="playlistAnchor songButton block"
+        onClick={() => {
+          history.push("/profile/1/overview");
+        }}
+      >
+        Ahmed{/*userName*/}
+      </button>
+      <button
+        onClick={playClicked}
+        data-testid="playButton"
+        className="playButton"
+        variant="outline-success"
+      >
+        {playing ? "PAUSE" : "PLAY"}
+      </button>
+      <p className="likedSongsTitle gray-text">
+        <span data-testid="songsNumber">{length} </span>
+        <span data-testid="songsLiteral">{length > 1 ? "songs" : "song"}</span>
+      </p>
+    </div>
+  );
 }
 
 HeaderBody.propTypes = {
-    length: PropTypes.number,
-    playing: PropTypes.bool,
-    playClicked: PropTypes.func
-}
-
+  length: PropTypes.number,
+  playing: PropTypes.bool,
+  playClicked: PropTypes.func
+};
 
 export default HeaderBody;
