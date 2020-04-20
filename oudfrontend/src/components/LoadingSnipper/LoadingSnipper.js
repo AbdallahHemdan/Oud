804f8e231d1e331c81940593356916683e7935cc
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-04-20 18:16:35 +0200

    🐛  Fix Loading Snipper warnings

--- oudfrontend/src/components/LoadingSnipper/LoadingSnipper.js
+++ oudfrontend/src/components/LoadingSnipper/LoadingSnipper.js
@@ -3,12 +3,12 @@ import "./LoadingSnipper.css"
 class LoadingSnipper extends Component {
   render() {
     return (
-      <div class="spinner">
-        <div class="rect1"></div>
-        <div class="rect2"></div>
-        <div class="rect3"></div>
-        <div class="rect4"></div>
-        <div class="rect5"></div>
+      <div className="spinner">
+        <div className="rect1"></div>
+        <div className="rect2"></div>
+        <div className="rect3"></div>
+        <div className="rect4"></div>
+        <div className="rect5"></div>
       </div>
     )
   }
