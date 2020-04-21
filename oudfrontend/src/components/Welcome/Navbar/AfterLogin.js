34fc05e1d8dc23d9c1f74dff5a809439c9e28ab9
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-04-21 16:07:53 +0200

    Refactor Main content in Welcome page

--- oudfrontend/src/components/Welcome/Navbar/AfterLogin.js
+++ oudfrontend/src/components/Welcome/Navbar/AfterLogin.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import mask from './../../../assets/images/mask.png';
+import axios from "axios"
+import { base, subUrl, prodUrl } from "./../../../config/environment"
+import { config } from "./../../../utils/auth"
+
+
+const fetchUserInfo = `${base}/me`;
+
 class AfterLogin extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      displayName: "",
+      images: []
+    }
+  }
+  handleStoringUserInfo = ({ displayName, images }) => {
+    this.setState({ displayName, images });
+    console.log("After Login Navbar state", this.state)
+  }
+  componentDidMount() {
+    axios.get(fetchUserInfo, config)
+      .then((result) => {
+        console.log(result.data);
+        this.handleStoringUserInfo(result.data);
+      }).catch((err) => {
+
+      });
+  }
+
   doLogOut = () => {
     localStorage.removeItem("accessToken");
   }
   render() {
+    const subPath = (base === prodUrl) ? subUrl : "";
     return (
-      <li class="nav-item dropdown">
+      <li className="nav-item dropdown">
         <Link
-          class="nav-link dropdown-toggle"
+          className="nav-link dropdown-toggle"
           id="navbarDropdown"
           role="button"
           data-toggle="dropdown"
@@ -18,25 +47,25 @@ class AfterLogin extends Component {
           to="/"
         >
           <img
-            className="img-responsive"
-            src={mask}
+            className="img-responsive user-image"
+            src={`${subPath}${this.state.images[0]}`}
             alt="Profile Icon"
             data-testid="profImage"
           />
-          <span>&nbsp;&nbsp;</span>Profile
-                    </Link>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <span>&nbsp;&nbsp;</span>{this.state.displayName}
+        </Link>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
           <Link
             data-testid="Account"
             to="/overview"
-            class="dropdown-item"
+            className="dropdown-item"
           >
             Account
                      </Link>
           <Link
             data-testid="LogOut"
             to="/signin"
-            class="dropdown-item"
+            className="dropdown-item"
             onClick={() => this.doLogOut()}
           >
             Log Out
