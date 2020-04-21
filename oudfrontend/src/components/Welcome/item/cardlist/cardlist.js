09fdda5b0e7792707058343a3f7b9262b063f51c
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-04-21 16:45:34 +0200

    🐛 Fix warning issue in CardList

--- oudfrontend/src/components/Welcome/item/cardlist/cardlist.js
+++ oudfrontend/src/components/Welcome/item/cardlist/cardlist.js
@@ -10,8 +10,8 @@ export const CardList = (props) => {
   return (
     <div className="container">
       <div className="cartList row ">
-        {props.MusicCard.map((MusicCard) => (
-          <Card data={MusicCard} />
+        {props.MusicCard.map((MusicCard, index) => (
+          <Card data={MusicCard} key={index} />
         ))}
       </div>
     </div>
