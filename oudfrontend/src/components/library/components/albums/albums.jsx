692ef7c57f0fd649fb75c16a9587a933a3d1bfa6
lido22 <ahmedcs2022@yahoo.com>
2020-04-22 15:31:47 +0200

    conditional rendring if recieved

--- oudfrontend/src/components/library/components/albums/albums.jsx
+++ oudfrontend/src/components/library/components/albums/albums.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { config } from '../../../../utils/auth'
+import { base } from '../../../../config/environment'
+import MusicCard from '../../../MusicCard/MusicCard'
+import LoadingSnipper from '../../../LoadingSnipper/LoadingSnipper';
 
 export class Albums extends Component {
+    constructor(){
+        super()
+        this.state = { albums:[],recieved:false}
+    }
+    componentDidMount() {
+        console.log(base);
+        axios
+          .get(`${base}/me/albums`, config)
+          .then((response) => {
+            this.setState({ albums: response.data.items });
+            this.setState({recieved:true})
+          })
+          .catch((error) => {
+            console.log(error.response);
+          });
+        }
     render() {
         return (
+
             <div>
                 <h1>Saved ALBUMS</h1>
+                {
+                (this.state.recieved)?this.state.albums.map(item =>{
+                   return(<MusicCard item={item.album}
+                    key={item._id}
+                    playBtn={true}
+                    />)
+               }): (
+                <LoadingSnipper/>
+                )
+               }
             </div>
         )
     }
--- oudfrontend/src/components/library/components/playlists/playlists.jsx
+++ oudfrontend/src/components/library/components/playlists/playlists.jsx
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { config } from '../../../../utils/auth'
+import { base } from '../../../../config/environment'
+import MusicCard from '../../../MusicCard/MusicCard'
+import LoadingSnipper from '../../../LoadingSnipper/LoadingSnipper';
+
 
 export class Playlists extends Component {
+    constructor(){
+        super()
+        this.state = { playlists:[] , recieved:false}
+    }
+    componentDidMount() {
+        console.log(base);
+        axios
+          .get(`${base}/me/playlists`, config)
+          .then((response) => {
+            this.setState({ playlists: response.data.items });
+            this.setState({recieved:true});
+          })
+          .catch((error) => {
+            console.log(error.response);
+          });
+        }
+        
     render() {
+        console.log(this.state);
         return (
             <div>
-                <h1> Followed Playlists </h1>
+                 <h1> Followed Playlists </h1>
+               
+               {(this.state.recieved)?this.state.playlists.map(item =>{
+                   return(<MusicCard item={item}
+                    key={item._id}
+                    playBtn={true}
+                    />)
+               }): <LoadingSnipper/>}
             </div>
         )
     }
