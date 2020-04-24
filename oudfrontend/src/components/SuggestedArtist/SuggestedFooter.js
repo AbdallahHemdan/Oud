4551e567ae21d886cb3904f8f5fa726966434b3e
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2020-04-24 04:59:38 +0200

    Refactor the Footer and make it conditional render

--- oudfrontend/src/components/SuggestedArtist/SuggestedFooter.js
+++ oudfrontend/src/components/SuggestedArtist/SuggestedFooter.js
@@ -2,27 +2,30 @@ import React, {Component} from 'react';
 // import {Link} from 'react-router-dom';
 import './suggestedArtist.css';
 import Axios from 'axios';
-import {base, subUrl, prodUrl} from './../../config/environment';
-import {config} from './../../utils/auth';
+import {base, subUrl, prodUrl} from '../../config/environment';
+import {config} from '../../utils/auth';
 
 const fetchUserInfo = `${base}/me/following ? type=artist`;
 
 class SuggestedFooter extends Component {
   constructor(props) {
     super(props);
-    this.state = {choo: true, selectedIDS: []};
+    this.state = {choo: true, selectedIDS: this.props.selectedIds};
   }
 
   handelOnClick = () => {
-    Axios.put(fetchUserInfo, config, this.state.selectedIDS)
+    Axios.put(fetchUserInfo, config, this.props.selectedIds)
       .then((response) => {
+        console.log(response);
         window.location = '/';
       })
-      .catch((error) => {});
+      .catch((error) => {
+        console.log(error.response);
+      });
   };
 
   render() {
-    return (
+    return this.props.selected() ? (
       <div className="SuggFooter">
         <footer className="page-footer font-small unique-color-dark pt-4">
           <div className="container">
@@ -33,6 +36,14 @@ class SuggestedFooter extends Component {
             >
               Finish
             </button>
+          </div>
+        </footer>
+      </div>
+    ) : (
+      <div className="SuggFooter">
+        <footer className="page-footer ">
+          <div className="container TextFotter" data-testid="Footertext">
+            <p>Select at least one artist or use the search bar.</p>
           </div>
         </footer>
       </div>
--- oudfrontend/src/pages/SuggestedArtistPage/SuggestedArtist.js
+++ oudfrontend/src/pages/SuggestedArtistPage/SuggestedArtist.js
@@ -1,15 +1,30 @@
 import React, {Component} from 'react';
 import SuggestedNavBar from './../../components/SuggestedArtist/SuggestedNavBar';
-import SuggestedFooterT from './../../components/SuggestedArtist/SuggestedFooterT';
-import SuggestedFooterB from './../../components/SuggestedArtist/SuggestedFooterB';
+import SuggestedFooter from './../../components/SuggestedArtist/SuggestedFooter';
 import MainArtistC from './../../components/SuggestedArtist/MainArtistC';
 export default class SuggestedArtist extends Component {
   constructor(props) {
     super(props);
-    this.state = {isSelected: false};
+    this.state = {isSelected: false, selectedIds: []};
   }
+  check = (sId, id) => {
+    return sId !== id;
+  };
+  componentDidUpdate() {}
+  handleSelect = (id, remove) => {
+    let ids = this.state.selectedIds;
+    if (!remove) {
+      ids.push(id);
+      this.setState({selectedIds: ids});
+    } else {
+      const selectedIds = this.state.selectedIds.filter((sId) =>
+        this.check(sId, id)
+      );
+      this.setState({selectedIds});
+    }
+  };
   isSelected = () => {
-    return this.state.isSelected ? true : false;
+    return this.state.selectedIds.length;
   };
   onClick = (event) => {
     this.setState({isSelected: event.target.value});
@@ -19,15 +34,18 @@ export default class SuggestedArtist extends Component {
   };
   handelRenderComp = () => {
     if (this.SuggestedFooterB.state.choo === true) {
-      
     }
   };
   render() {
     return (
       <div>
         <SuggestedNavBar />
-        <MainArtistC />
-        {this.isSelected() ? <SuggestedFooterB /> : <SuggestedFooterT />}
+        <MainArtistC handleSelect={this.handleSelect} />
+        <SuggestedFooter
+          selected={this.isSelected}
+          selectedIds={this.state.selectedIds}
+        />
+        )}
       </div>
     );
   }
