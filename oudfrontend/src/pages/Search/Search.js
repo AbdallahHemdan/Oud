288150a8297763636827eb82f09dbe8c8fde2819
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2020-06-10 02:11:47 +0200

    Add Footer unit test

--- oudfrontend/src/pages/Search/Search.js
+++ oudfrontend/src/pages/Search/Search.js
@@ -1,48 +1,49 @@
-import React, { Component } from 'react'
-import axios from "axios"
-import Sidebar from "../../components/Sidebar/Sidebar"
-import Navbar from "../../components/Navbar/Navbar"
-import BrowseAll from "./../../components/BrowseAll/BrowseAll"
+import React, {Component} from 'react';
+import axios from 'axios';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Navbar from '../../components/Navbar/Navbar';
+import BrowseAll from './../../components/BrowseAll/BrowseAll';
 import RecentSearch from './../../components/RecentSearch/RecentSearch';
-import { base } from "./../../config/environment"
+import {base} from './../../config/environment';
 import LoadingSnipper from './../../components/LoadingSnipper/LoadingSnipper';
-import { isLoggedIn } from '../../utils/auth'
-import SearchAfterTyping from "./../../components/SearchAfterTyping/SearchAfterTyping"
-import "./Search.css"
+import {isLoggedIn} from '../../utils/auth';
+import SearchAfterTyping from './../../components/SearchAfterTyping/SearchAfterTyping';
+import './Search.css';
 
 /**
  * fetch url of the categories
  * @type {string}
+ * @author Abdallah Hemdan
  */
 
 let fetchCategoriesUrl = `${base}/browse/categories`;
 class Search extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       /**
        * An array of all categories of the music to display it in home page
-       * 
-       * @type {Array<object>} 
+       *
+       * @type {Array<object>}
        */
       items: [],
 
       /**
        * The maximum number of categories to get.
-       * 
+       *
        * @type {number}
        */
       limit: 0,
 
       /**
        * The index of the first categories to get.
-       * 
+       *
        * @type {number}
        */
       offset: 0,
       /**
        * The total number of categories available to get.
-       * 
+       *
        * @type {number}
        */
       total: 0,
@@ -55,9 +56,9 @@ class Search extends Component {
        * the value of the search that the user has type
        * @type {String}
        */
-      search: "",
+      search: '',
       /**
-       * Variable to determine if the request to get what user type 
+       * Variable to determine if the request to get what user type
        * @type {Boolean}
        */
       canSend: false,
@@ -70,44 +71,44 @@ class Search extends Component {
        * Variable to check if the user is already logged in or not
        * @type {Boolean}
        */
-      isLoggedIn: isLoggedIn()
-    }
+      isLoggedIn: isLoggedIn(),
+    };
   }
   /**
    * Function to handle storing fetched categories in the state
-   * 
+   *
    * @function
-   * 
+   *
    * @returns {void}
-   * 
+   *
    */
-  handleStoringPlaylists = ({ items, limit, offset, total }) => {
-    this.setState({ items, limit, offset, total, isLoading: false });
-  }
+  handleStoringPlaylists = ({items, limit, offset, total}) => {
+    this.setState({items, limit, offset, total, isLoading: false});
+  };
   /**
    * Function to handle store search value that user type in the state..
-   * 
+   *
    * @function
-   * 
+   *
    * @returns {void}
    */
   handleInput = (e) => {
-    console.log("Handle Input", e.target.value);
+    console.log('Handle Input', e.target.value);
     let search = e.target.value;
-    this.setState(prevState => {
-      return { ...prevState, search }
+    this.setState((prevState) => {
+      return {...prevState, search};
     });
-  }
+  };
   /**
    * Function to handle enable can send state of allowing perform the request
    * @function
    *
    * @return {void}
    *
-  */
+   */
   handleStoringCanSend = () => {
-    this.setState({ canSend: true });
-  }
+    this.setState({canSend: true});
+  };
   /**
    * Function to handle of setting the time out to delay half of second after the user stop typing to do our search
    * @function
@@ -118,11 +119,9 @@ class Search extends Component {
   handleKeyUp = () => {
     clearTimeout(this.state.typingTimeout);
     this.setState({
-      typingTimeout: setTimeout(
-        this.handleStoringCanSend
-        , 500)
+      typingTimeout: setTimeout(this.handleStoringCanSend, 500),
     });
-  }
+  };
   /**
    * Function to handle setting canSent state to false while the user is still typing
    * @function
@@ -131,8 +130,8 @@ class Search extends Component {
    *
    */
   handleKeyDown = () => {
-    this.setState({ canSend: false });
-  }
+    this.setState({canSend: false});
+  };
   /**
    * Function to prevent default behavior of the submit and disable the refreshing of the page
    * @function
@@ -142,17 +141,19 @@ class Search extends Component {
    */
   handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
   /**
    * Fetch all the Categories of Browse all and store it in the state
    *
    * @returns {void} - nothing to return, it just fetch data and set it in the state
    */
   componentDidMount() {
-    axios.get(fetchCategoriesUrl)
+    axios
+      .get(fetchCategoriesUrl)
       .then((result) => {
         this.handleStoringPlaylists(result.data);
-      }).catch((err) => {
+      })
+      .catch((err) => {
         console.log(err);
       });
   }
@@ -162,13 +163,11 @@ class Search extends Component {
    * @description Render all the search page components
    *
    * @returns {JSX} Component for Home
-  */
+   */
   render() {
     return (
       <React.Fragment>
-        <Sidebar
-          data-testid="sidebar"
-        />
+        <Sidebar data-testid="sidebar" />
         <Navbar
           isLoggedIn={isLoggedIn()}
           isSearch={true}
@@ -179,56 +178,37 @@ class Search extends Component {
           onKeyDown={this.handleKeyDown}
           data-testid="navbar"
         />
-        {
-          this.state.isLoading ?
-            (
-              <LoadingSnipper
-                data-testid="loading-snipper"
-              />
-            ) :
-            (
-              <section
-                className="main-content"
-                data-testid="main-content"
-              >
-                <section
-                  className="music-component main"
-                  data-testid="music-content"
-                >
-                  {
-                    (this.state.search !== "") ?
-                      (
-                        <SearchAfterTyping
-                          data-testid="search-after-typing"
-                          search={this.state.search}
-                          canSend={this.state.canSend}
-                        />
-                      )
-                      :
-                      (
-                        <div
-                          data-testid="search-before-typing"
-                        >
-                          {
-                            this.state.isLoggedIn ?
-                              <RecentSearch
-                                data-testid="recent-search"
-                              /> : null
-                          }
-                          <BrowseAll
-                            items={this.state.items}
-                            data-testid="browse-all"
-                          />
-                        </div>
-                      )
-                  }
-                </section>
-              </section>
-            )
-        }
+        {this.state.isLoading ? (
+          <LoadingSnipper data-testid="loading-snipper" />
+        ) : (
+          <section className="main-content" data-testid="main-content">
+            <section
+              className="music-component main"
+              data-testid="music-content"
+            >
+              {this.state.search !== '' ? (
+                <SearchAfterTyping
+                  data-testid="search-after-typing"
+                  search={this.state.search}
+                  canSend={this.state.canSend}
+                />
+              ) : (
+                <div data-testid="search-before-typing">
+                  {this.state.isLoggedIn ? (
+                    <RecentSearch data-testid="recent-search" />
+                  ) : null}
+                  <BrowseAll
+                    items={this.state.items}
+                    data-testid="browse-all"
+                  />
+                </div>
+              )}
+            </section>
+          </section>
+        )}
       </React.Fragment>
     );
   }
 }
 
-export default Search
+export default Search;
