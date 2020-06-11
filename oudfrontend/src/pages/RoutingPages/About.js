58a49ee7cf52a8855c7beb4e10d18fe0fdc5bb08
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-11 12:27:12 +0200

    Remove dummy pages

--- oudfrontend/src/pages/RoutingPages/About.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function About() {
-  return <h1>Dummy page for About Page</h1>;
-}
-
-export default About;
--- oudfrontend/src/pages/RoutingPages/premium.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Download() {
-  return <h1>premium</h1>;
-}
-
-export default Download;
